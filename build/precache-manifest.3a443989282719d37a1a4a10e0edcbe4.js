b9da8a85463e41a5550a18e2986e98c8265b69f6
David Thorpe <davidthorpe878@gmail.com>
2020-06-06 20:13:30 +0100

    First commit

---
+++ build/precache-manifest.3a443989282719d37a1a4a10e0edcbe4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe09411f1f98e8b597372928702730f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4112c9cc2ac64564c054",
+    "url": "/static/js/2.bf06e444.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bf06e444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22bbdb5367af4e7cca7f",
+    "url": "/static/js/main.47f6f261.chunk.js"
+  },
+  {
+    "revision": "ac72383ba3da5684c078",
+    "url": "/static/js/runtime-main.75e19d5c.js"
+  }
+]);
---
+++ src/pages/Create.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Create = () => {
+  console.log("Create page");
+  return (
+    <div>
+      <h1>Create</h1>
+    </div>
+  )
+}
+
+export default Create;
